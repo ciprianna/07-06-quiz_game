6dcb53af0875b11e1c8b94a62d1fd5222a6bebbb
Ciprianna <ciprianna17@gmail.com>
2015-07-07 10:19:02 -0500

    Attempt to create question as an Object and push it to an Array.   Untested as of yet.

--- global.js
+++ global.js
@@ -8,3 +8,14 @@ var PleaseAnswer = toLowerCase(Please)
 while (PleaseAnswer != a || PleaseAnswer != b || PleaseAnswer != c || PleaseAnswer != d) {
   var Please = prompt("Answer (A, B, C, or D)", "");
 }
+
+// As an Object
+// Create the Array to store Questions
+var Questions = new.Array();
+
+// First question
+var DasIsPlease = new.Object("Translate to German: 'please'\nA) Verzeihung\nB) Bitte\nC) Danke\nD) Tschau");
+DasIsPlease.answer = "bitte";
+DasIsPlease.choices = ["verzeihung", "bitte", "danke", "tschau"];
+// Push the question to the Array
+Questions.push(DasIsPlease);
